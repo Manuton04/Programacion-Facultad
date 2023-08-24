9944d858dcd3517cc323122a055c6ad9c5c10d0b
Manuton04 <manutorres004@gmail.com>
2023-08-24 19:54:39 -0300

    Termine El Ejercicio 1

--- Introducción a la programacion II/1-Archivos/Ejercicio1.pas
+++ Introducción a la programacion II/1-Archivos/Ejercicio1.pas
@@ -1,9 +1,9 @@
 Program Ejercicio1;
 
 type
-    ArchivoEnteros= File of Integer;
+    TArchivoEnteros= File of Integer;
 
-procedure AbrirArc(var arch: ArchivoEnteros; var error: boolean);
+procedure AbrirArc(var arch: TArchivoEnteros; var error: boolean);
 begin
     error:= false;
     Assign(arch, '/work/MTorres_Enteros.dat');
@@ -14,26 +14,69 @@ begin
         error:= true;
 end;
 
+procedure EscribirArc(var arch: TArchivoEnteros);
+var n: Integer;
+begin
+    Seek(arch, 0);
+    while (not eof(arch)) do begin
+        Read(arch, n);
+        writeln('El valor en la posición ', Filepos(arch), ' es: ', n);
+    end;
+end;
+
+function Promediar(var arch: TArchivoEnteros): Real;
+var sum, val, n: Integer;
+begin
+    sum:= 0;
+    n:= 0;
+    Seek(arch, 0);
+    while not eof(arch) do begin
+        read(arch, val);
+        sum:= sum + val;
+        n:= n + 1;
+    end;
+    Promediar:= sum / n;
+end;
+
+function Sumar(var arch: TArchivoEnteros): Integer;
+var sum, val: Integer;
+begin
+    sum:= 0;
+    Seek(arch, 0);
+    while not eof(arch) do begin
+        read(arch, val);
+        sum:= sum + val;
+    end;
+    Sumar:= sum;
+end;
+
 var
-    Arc_Enteros: ArchivoEnteros;
+    Arc_Enteros: TArchivoEnteros;
     Num, i: Integer;
     Error: boolean;
 
 begin
     AbrirArc(Arc_Enteros, Error);
-        if not Error then begin
-            for i:= 1 to 3 do begin
-                Write('Agregue el valor número '+ i+ ': ');
-                ReadLn(Num);
-                write(Arc_Enteros, Num);
-            end;
-            while (not eof(Arc_Enteros)) do begin
-                read(Arc_Enteros, Num);
-                writeln('El valor número '+ (Filepos(Arc_Enteros)+1)+ ' es: '+ Num);
-            end;
-            close(Arc_Enteros);
+    if not Error then begin
+            Seek(Arc_Enteros, FileSize(Arc_Enteros));
+            Write('Agregue el valor del elemento ', (FileSize(Arc_Enteros)+1),': ');
+            ReadLn(Num);
+            write(Arc_Enteros, Num);
+        EscribirArc(Arc_Enteros);
+        WriteLn('');
+        writeLn('El promedio de los valores es: ', Promediar(Arc_Enteros):0:2);
+        writeLn('La suma de los valores es: ', Sumar(Arc_Enteros));
+        close(Arc_Enteros);
+    end
+    else begin
+        rewrite(Arc_Enteros);
+        for i:= 1 to 3 do begin
+            Write('Agregue el valor del elemento ', i, ': ');
+            ReadLn(Num);
+            write(Arc_Enteros, Num);
         end;
-
-    
-
+        EscribirArc(Arc_Enteros);
+        close(Arc_Enteros);
+    end;
+    Erase(Arc_Enteros);
 end.
